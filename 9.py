a6f41a7cf433b05e77c630bfeda54a77e0d3969a
Jagddish <jagddish13@gmail.com>
2021-10-30 14:43:30 +0530

    range()

---
+++ 9.py
@@ -0,0 +1,11 @@
+# page 56
+# range() 
+
+for i in range(12, 16):
+    print(i)
+
+for i in range(0, 10, 2): #this will print 0,9 skipping 2 numbers
+    print(i)
+
+for i in range(5, -1, -1):
+    print(i)
