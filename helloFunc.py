4d068ff5e941e7d94b7c43287720128f5d8fbd3d
Jagddish <jagddish13@gmail.com>
2021-10-30 14:56:57 +0530

    Functions

---
+++ helloFunc.py
@@ -0,0 +1,13 @@
+# page 62 
+# 3 Functions
+
+def hello():  # creating a function named hello 
+    print('Howdy!') # inside func we're printing
+    print('Howdy!!!') # when we call this func all the code inside 
+    print('Hello there.') # this func will execute
+
+hello() # calling func  by name() 
+hello()
+hello()
+
+
