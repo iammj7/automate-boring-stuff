6de278452a340f8613705f7adb464b046524cbb5
Jagddish <jagddish13@gmail.com>
2021-10-30 15:35:34 +0530

    Functions

---
+++ helloFunc2.py
@@ -0,0 +1,12 @@
+# page 63
+
+
+def hello(name):  # passing argument
+    print("Hello " + name)
+
+
+# since we created our function with a arg we hv to give that arg
+# when we calling the function
+
+hello("bob")
+hello("Alice")
