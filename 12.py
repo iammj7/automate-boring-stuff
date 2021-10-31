66d5ed645d4ab5e135080e3e6225ffa6489fa123
Jagddish <jagddish13@gmail.com>
2021-10-31 22:13:31 +0530

    printing with continue

---
+++ 12.py
@@ -0,0 +1,15 @@
+print("hello")
+print("World")
+# both lines are printed separately
+
+# now both lines printed on the same line
+print("Hello", end='')
+print("World")
+
+# this just prints the items without ,
+print("cats", "dogs", "mice")
+
+#we can add comma in the print usng sep=','
+print('cats', 'dogs', 'mice', sep=',')
+
+
