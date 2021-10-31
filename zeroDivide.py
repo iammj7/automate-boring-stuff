081c33393a8664c401b45e869aaa0e7df7dd0eb8
Jagddish <jagddish13@gmail.com>
2021-10-31 22:23:30 +0530

    try except

---
+++ zeroDivide.py
@@ -0,0 +1,16 @@
+# page 73
+
+def spam(dividedBy):
+    try:
+
+        return 42/ divideBy
+    except ZeroDivisionError:
+        print('Error: Invalid argument.')
+
+
+print(spam(2))
+print(spam(12))
+print(spam(0))
+print(spam(1))
+# a Zero devision error happesn whenever you try to divide with zero
+# we can excepct this error telling python to ignore it
